857559512449477dab989a79e86c4cf97c551c5e
David Rama Jimeno <david.ramajimeno@tuni.fi>
2024-01-26 17:07:08 +0200

    Description of the gate.vhd

--- E02/ripple_carry_adder.vhd
+++ E02/ripple_carry_adder.vhd
@@ -51,14 +51,24 @@ architecture gate of ripple_carry_adder is
 begin  -- gate
 
 
-  Carry_ha <= unsigned(a_in(0)) and unsigned(b_in(0));
-  C <= unsigned(a_in(1)) xor unsigned(b_in(1));
-  D <= Carry_ha and C;
-  E <= unsigned(a_in(1)) and unsigned(b_in(1));
+  Carry_ha <= unsigned(a_in(0)) and unsigned(b_in(0)); 
+  -- If a_in(0) and b_in(0) are both 1, when we add them we store the carry value in this variable, 
+  -- which will affect the next bits of s_out.   
+  C <= unsigned(a_in(1)) xor unsigned(b_in(1));       
+  -- It is the result of adding a_in(1) and b_in(1), but if the Carry_ha is also 1, it will affect the Carry_fa
+  D <= Carry_ha and C;                                 
+  -- If the C sum and the Carry_ha are 1, it will affect the value of Carry_fa, because we have an extra bit.
+  E <= unsigned(a_in(1)) and unsigned(b_in(1));        
+  -- If both a_in(1) and b_in(1) are 1, the carry value will be sent to Carry_fa through E
   Carry_fa <= D or E;
-  F <= unsigned(a_in(2)) xor unsigned(b_in(2));
-  G <= Carry_fa and F;
-  H <= unsigned(a_in(2)) and unsigned(b_in(2));
+  -- Carry_fa carries the extra bit value of the sum of the first two bits of a_in and b_in
+  F <= unsigned(a_in(2)) xor unsigned(b_in(2));        
+  -- It is the result of adding a_in(2) and b_in(2), but if the Carry_fa is also 1, it will affect s_out(3)
+  G <= Carry_fa and F;     
+  -- If the F sum and the Carry_fa are 1, it will affect the value of s_out(3), because we have an extra bit                  
+  H <= unsigned(a_in(2)) and unsigned(b_in(2));       
+  -- If both a_in(2) and b_in(2) are 1, the carry value will be sent to s_out(3) through E
+
 
   s_out(0) <= std_logic(unsigned(a_in(0)) xor unsigned(b_in(0)));
   s_out(1) <= std_logic(C xor Carry_ha);
