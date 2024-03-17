3aa3727d69f813c87d092ab94b45abec7d879448
zian <nouman.zia@tuni.fi>
2024-03-17 02:14:36 +0200

    EX08

---
+++ E08/audio_codec_model.vhd
@@ -0,0 +1,125 @@
+-------------------------------------------------------------------------------
+-- Title      : COMP.CE.240 Logic Synthesis, Exercise 08
+-------------------------------------------------------------------------------
+-- File       : audio_codec_model.vhd
+-- Author     : Nouman Zia, David Rama Jimeno
+-- Group number : 6
+-- Company    : TUNI
+-- Created    : 2024-02-21
+-- Platform   : 
+-- Standard   : VHDL'87
+-------------------------------------------------------------------------------
+-- Description: A a model implementation for the audio codec using state machine
+-------------------------------------------------------------------------------
+
+-- Include default libraries
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Declare entity audio_codec_model
+ENTITY audio_codec_model IS
+
+    generic (
+        data_width_g : integer := 16
+        );
+    PORT (
+        aud_bclk_in : in std_logic; -- works as a clock
+        rst_n : in std_logic;
+        aud_data_in : in std_logic;
+        aud_lrclk_in : in std_logic;
+        value_left_out  : out std_logic_vector(data_width_g-1 DOWNTO 0);
+        value_right_out   : out std_logic_vector(data_width_g-1 DOWNTO 0)
+        );   
+END audio_codec_model;
+
+
+architecture rtl_mealy_1_reg of audio_codec_model is
+
+    -- Enumerate possible states with human readable names
+    type states_type is (wait_for_input, read_left, read_right);
+    signal curr_state_r : states_type;
+    signal next_state   : states_type;
+
+    -- counter to stop storing input data 
+    SIGNAL counter_r  : signed(data_width_g-1 DOWNTO 0); 
+
+    -- output registers
+    signal left_data_r  : std_logic_vector(data_width_g-1 DOWNTO 0);
+    signal right_data_r  : std_logic_vector(data_width_g-1 DOWNTO 0);
+
+    -- internal signals
+    signal value_left  : std_logic_vector(data_width_g-1 DOWNTO 0);
+    signal value_right  : std_logic_vector(data_width_g-1 DOWNTO 0);
+
+begin  -- rtl_mealy_1_reg
+    value_left_out <= value_left;
+    value_right_out <= value_right;
+    -- This only process handles the state changes as well as output.
+    -- Hence, output will be registered. It will change simultaneously with
+    -- curr_state_r in this example.
+
+    single : process (aud_bclk_in, rst_n)
+    
+    begin   -- process single
+        if rst_n = '0' then     -- asynchronous reset (active low)
+            curr_state_r <= wait_for_input;     -- init state
+            left_data_r <= (OTHERS => '0');
+            right_data_r <= (OTHERS => '0');
+            counter_r <= (OTHERS => '0');
+            value_left <= (OTHERS => '0');
+            value_right <= (OTHERS => '0');
+    
+        elsif aud_bclk_in'event and aud_bclk_in = '1' then  -- rising clock edge
+            -- FSM always checks what is the current state
+            -- Here that is done with case-clause
+            case curr_state_r is
+
+                when wait_for_input =>
+                    if aud_lrclk_in = '1' then -- lrclk raising edge
+                        left_data_r <= left_data_r(data_width_g-2 downto 0) 
+                        & aud_data_in; -- serial to parallel data transfer by left shift
+                        curr_state_r <= read_left;
+                    end if;
+                    
+                when read_left =>
+                    if counter_r + 1 /= data_width_g then
+                        left_data_r <= left_data_r(data_width_g-2 downto 0) 
+                        & aud_data_in; -- serial to parallel data transfer by left shift
+                        counter_r <= counter_r + 1;
+                    end if;
+                    if counter_r + 1 = data_width_g then
+                        value_left <= left_data_r; -- 
+                    end if;
+                    
+                    if aud_lrclk_in = '0' then -- lrclk falling edge
+                        counter_r <= (OTHERS => '0');
+                        left_data_r <= (OTHERS => '0');
+                        right_data_r <= right_data_r(data_width_g-2 downto 0) 
+                        & aud_data_in; -- serial to parallel data transfer by left shift
+                        curr_state_r <= read_right;
+                    end if;
+                    
+                when read_right =>
+                    if counter_r + 1 /= data_width_g then
+                        right_data_r <= right_data_r(data_width_g-2 downto 0)
+                         & aud_data_in; -- serial to parallel data transfer by left shift
+                        counter_r <= counter_r + 1;
+                    end if;
+                    if counter_r + 1 = data_width_g then
+                        value_right <= right_data_r;
+                    end if;                        
+                    if aud_lrclk_in = '1' then -- lrclk raising edge
+                        right_data_r <= (OTHERS => '0');
+                        counter_r <= (OTHERS => '0');
+                        left_data_r <= left_data_r(data_width_g-2 downto 0) & aud_data_in; -- joining input data by shifting right
+                        curr_state_r <= read_left;
+                    end if;
+            end case;
+        end if;                       
+    
+    end process single;
+
+end rtl_mealy_1_reg;
+
+
---
+++ E08/tb_audio_ctrl.vhd
@@ -0,0 +1,170 @@
+-------------------------------------------------------------------------------
+-- Title      : COMP.CE.240 Logic Synthesis, Exercise 07
+-------------------------------------------------------------------------------
+-- File       : tb_audio_ctrl.vhd
+-- Author     : Nouman Zia, David Rama Jimeno
+-- Group number : 6
+-- Company    : TUNI
+-- Created    : 2024-03-05
+-- Platform   : 
+-- Standard   : VHDL'87
+-------------------------------------------------------------------------------
+-- Description: Test bench to validate the functionality of audio controller
+-------------------------------------------------------------------------------
+
+-- Include default libraries
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Declare entity
+ENTITY tb_audio_ctrl IS
+
+END tb_audio_ctrl;
+
+-- Architecture called 'testbench' is  defined below
+ARCHITECTURE testbench of tb_audio_ctrl IS
+
+-- set the clock period
+CONSTANT period_c : TIME := 50 ns; -- 50ns = 20 MHz
+
+-- Set the time when generator is cleared synchronously
+CONSTANT clear_delay_c    : INTEGER := 74000;
+CONSTANT clear_duration_c : INTEGER := 6000;
+
+-- set data width and waveform frequency for wave generators
+CONSTANT data_width_c : INTEGER := 16;
+CONSTANT stepl_c    : INTEGER := 2;
+CONSTANT stepr_c    : INTEGER := 10;
+
+-- audio controller parameters
+CONSTANT ref_clk_freq_c : INTEGER := 20000000; -- 50 ns
+CONSTANT sample_rate_c  : INTEGER := 48000; -- lrclk
+
+-- internal signals
+SIGNAL clk : std_logic := '0'; -- init values only in tb
+SIGNAL rst_n : std_logic := '0'; -- init values only in tb   
+SIGNAL l_data_wg_actrl : std_logic_vector(data_width_c-1 DOWNTO 0);
+SIGNAL r_data_wg_actrl : std_logic_vector(data_width_c-1 DOWNTO 0);
+SIGNAL aud_bit_clk : std_logic; 
+SIGNAL aud_lr_clk : std_logic;
+SIGNAL aud_data : std_logic;
+SIGNAL l_data_codec_tb : std_logic_vector(data_width_c-1 DOWNTO 0);
+SIGNAL r_data_codec_tb : std_logic_vector(data_width_c-1 DOWNTO 0);
+SIGNAL sync_clear : std_logic; 
+
+    -- Define components 
+    
+    -- waveform generation component
+    COMPONENT wave_gen
+        generic (
+        width_g : integer; -- counter width
+        step_g : integer -- counter step
+        );
+        PORT (
+        clk : in std_logic;
+        rst_n : in std_logic;
+        sync_clear_n_in : in std_logic;
+        value_out : out std_logic_vector(width_g-1 DOWNTO 0)
+        );   
+    END COMPONENT;
+    -- audio controller component
+    COMPONENT audio_ctrl
+        generic (
+            ref_clk_freq_g : integer; -- frequency of lrclk
+            sample_rate_g : integer; -- frequency of bclk
+            data_width_g : integer
+            );
+        PORT (
+            clk : in std_logic;
+            rst_n : in std_logic;
+            left_data_in  : in std_logic_vector(data_width_g-1 DOWNTO 0);
+            right_data_in   : in std_logic_vector(data_width_g-1 DOWNTO 0);
+            aud_bclk_out  : out std_logic; -- Bit clock
+            aud_data_out  : out std_logic;
+            aud_lrclk_out  : out std_logic -- Left-right clock
+            );   
+    END COMPONENT;
+    -- audio codec model component
+    COMPONENT audio_codec_model
+    generic (
+        data_width_g : integer
+        );
+    PORT (
+        aud_bclk_in : in std_logic; -- works as a clock
+        rst_n : in std_logic;
+        aud_data_in : in std_logic;
+        aud_lrclk_in : in std_logic;
+        value_left_out  : out std_logic_vector(data_width_g-1 DOWNTO 0);
+        value_right_out   : out std_logic_vector(data_width_g-1 DOWNTO 0)
+        );   
+    END COMPONENT;
+
+
+begin -- testbench architecture
+
+    -- create clock and reset signals
+    clk <= NOT clk AFTER period_c/2; -- this style needs init value
+    rst_n <= '0', '1' AFTER 4*period_c;
+    -- Create synchronous clear signal
+    sync_clear <= '1',
+    '0' AFTER period_c*clear_delay_c,
+    '1' AFTER period_c*(clear_delay_c+clear_duration_c);
+  
+    --l_data_wg_actrl <= "1110101010101011";
+    --r_data_wg_actrl <= "1110101010101011";
+    -- Port mappings
+    left_channel : wave_gen
+        generic map (
+        width_g => data_width_c,
+        step_g => stepl_c
+        )
+        PORT map (
+        clk => clk, 
+        rst_n => rst_n, 
+        sync_clear_n_in => sync_clear, 
+        value_out => l_data_wg_actrl
+        );
+    
+    right_channel : wave_gen
+        generic map (
+        width_g => data_width_c,
+        step_g => stepr_c
+        )
+        PORT map (
+        clk => clk, 
+        rst_n => rst_n, 
+        sync_clear_n_in => sync_clear, 
+       value_out => r_data_wg_actrl
+        );
+    
+    controller : audio_ctrl
+        generic map (
+        ref_clk_freq_g => ref_clk_freq_c, -- frequency of lrclk
+        sample_rate_g => sample_rate_c, -- frequency of bclk
+        data_width_g => data_width_c
+        )
+        PORT map (
+        clk => clk,
+        rst_n => rst_n,
+        left_data_in => l_data_wg_actrl,
+        right_data_in => r_data_wg_actrl,
+        aud_bclk_out => aud_bit_clk,
+        aud_data_out => aud_data,
+        aud_lrclk_out => aud_lr_clk
+        );
+
+    model: audio_codec_model
+        generic map (
+        data_width_g => data_width_c
+        )
+        PORT map (
+        aud_bclk_in => aud_bit_clk, -- works as a clock
+        rst_n => rst_n,
+        aud_data_in => aud_data,
+        aud_lrclk_in => aud_lr_clk,
+        value_left_out => l_data_codec_tb,
+        value_right_out => r_data_codec_tb
+        );
+
+end testbench;
