8ba7455527794d1b1a5d54c61ba2708f3abf6b59
David Rama Jimeno <david.ramajimeno@tuni.fi>
2024-04-29 09:37:28 +0300

    Update i2c_config.vhd

--- E12/i2c_config.vhd
+++ E12/i2c_config.vhd
@@ -44,40 +44,39 @@ ARCHITECTURE rtl of i2c_config is
 
 -- Define internal SIGNALs and constants
 
-signal counter_sclk : integer range 0 to 10000;
+signal counter_sclk_r : integer range 0 to 10000;
 signal sclk_r : std_logic;
 signal sdat_r : std_logic;
-signal wait_done : std_logic;
-signal start_stop_done : std_logic;
-signal wait_counter : integer range 0 to 10000;
---signal start_stop_counter : integer;
-signal bits_counter : integer range 0 to 10000;
-signal byte_counter : integer range 0 to 10000;
-signal param_counter : integer range 0 to 10000;
-signal ack_flag : std_logic;
-signal stop_request : std_logic;
-signal nack : std_logic;
-signal nack_done : std_logic;
-constant data_width : integer := 8;
-constant bytes_sent : integer := 3;
-constant wait_delay : integer := 5000;
-constant start_stop_delay : integer := 5000;
+signal wait_done_r : std_logic;
+signal start_stop_done_r : std_logic;
+signal wait_counter_r : integer range 0 to 10000;
+signal bits_counter_r : integer range 0 to 10000;
+signal byte_counter_r : integer range 0 to 10000;
+signal param_counter_r : integer range 0 to 10000;
+signal ack_flag_r : std_logic;
+signal stop_request_r : std_logic;
+signal nack_r : std_logic;
+signal nack_done_r : std_logic;
+constant data_width_c : integer := 8;
+constant bytes_sent_c : integer := 3;
+constant wait_delay_c : integer := 5000;
+constant start_stop_delay_c : integer := 5000;
 
-type data_type is array (n_params_g-1 downto 0) of std_logic_vector(data_width*bytes_sent-1 downto 0);
+type data_type is array (n_params_g-1 downto 0) of std_logic_vector(data_width_c*bytes_sent_c-1 downto 0);
 type state is (wait_start,start,write_data,ack,stop,finish);
 
 signal curr_state_r : state;
-signal data_register_r : std_logic_vector(data_width*bytes_sent - 1 downto 0);
-signal byte_register_r : std_logic_vector(data_width - 1 downto 0);
+signal data_register_r : std_logic_vector(data_width_c*bytes_sent_c - 1 downto 0);
+signal byte_register_r : std_logic_vector(data_width_c - 1 downto 0);
 
-constant data_transfer : data_type := ("001101000001110110000000", "001101000010011100000100",
-                                       "001101000010001000001011", "001101000010100000000000", 
-                                       "001101000010100110000001", "001101000110100100001000",
-                                       "001101000110101000000000", "001101000100011111100001", 
-                                       "001101000110101100001001", "001101000110110000001000", 
-                                       "001101000100101100001000", "001101000100110000001000", 
-                                       "001101000110111010001000", "001101000110111110001000", 
-                                       "001101000101000111110001");
+constant data_transfer_c : data_type := ("001101000001110110000000", "001101000010011100000100",
+                                         "001101000010001000001011", "001101000010100000000000", 
+                                         "001101000010100110000001", "001101000110100100001000",
+                                         "001101000110101000000000", "001101000100011111100001", 
+                                         "001101000110101100001001", "001101000110110000001000", 
+                                         "001101000100101100001000", "001101000100110000001000", 
+                                         "001101000110111010001000", "001101000110111110001000", 
+                                         "001101000101000111110001");
 begin -- rtl
 
   sclk_out <= sclk_r;
@@ -87,17 +86,17 @@ begin -- rtl
 
     if (rst_n = '0') then
       sclk_r <= '1';
-      counter_sclk <= 0;
+      counter_sclk_r <= 0;
     elsif (clk'EVENT and clk='1') then
-      if counter_sclk = (ref_clk_freq_g / i2c_freq_g)/2-1 then
+      if counter_sclk_r = (ref_clk_freq_g / i2c_freq_g)/2-1 then
         if curr_state_r = write_data or curr_state_r = ack then -- The clock operates only in these states
           sclk_r <= not sclk_r;
         else 
           sclk_r <= '1';  -- After the clock cycle it remains low and goes to wait_start
         end if;
-        counter_sclk <= 0;
+        counter_sclk_r <= 0;
       else
-        counter_sclk <= counter_sclk+1;
+        counter_sclk_r <= counter_sclk_r+1;
       end if;
     end if;
 
@@ -109,26 +108,28 @@ begin -- rtl
 
     if (rst_n = '0') then
       sdat_r <= '1';
-      sdat_inout <= 'Z';
+      sdat_inout <= '1';
       finished_out <= '0';
       curr_state_r <= wait_start;
-      wait_done <= '0';
-      start_stop_done <= '0';
-      ack_flag <= '0';
-      stop_request <= '0';
-      nack <= '0';
-      nack_done <= '0';
-      param_counter <= 0;
-      byte_counter <= 0;
-      bits_counter <= 0;
-      wait_counter <= 0;
+      wait_done_r <= '0';
+      start_stop_done_r <= '0';
+      ack_flag_r <= '0';
+      stop_request_r <= '0';
+      nack_r <= '0';
+      nack_done_r <= '0';
+      param_counter_r <= 0;
+      byte_counter_r <= 0;
+      bits_counter_r <= 0;
+      wait_counter_r <= 0;
+      data_register_r <= (OTHERS => '0');
+      byte_register_r <= (OTHERS => '0');
     elsif (clk'EVENT and clk='1') then
       case curr_state_r is
 -----------------------------------------------------------------------
                          --WAIT_START--
 -----------------------------------------------------------------------
         when wait_start =>
-          if wait_done = '1' and rst_n = '1' then -- We wait until the reset signal is high and we waited the buffer time
+          if wait_done_r = '1' and rst_n = '1' then -- We wait until the reset signal is high and we waited the buffer time
             curr_state_r <= start;
           else
             curr_state_r <= wait_start;
@@ -137,7 +138,7 @@ begin -- rtl
                          --START--
 -----------------------------------------------------------------------
         when start =>
-          if wait_done = '1' and start_stop_done = '1' then -- In this state we pull the sdat line to low
+          if wait_done_r = '1' and start_stop_done_r = '1' then -- In this state we pull the sdat line to low
             curr_state_r <= write_data;
           else
             curr_state_r <= start;
@@ -146,8 +147,8 @@ begin -- rtl
                         --WRITE_DATA--
 -----------------------------------------------------------------------
         when write_data =>
-          if bits_counter = data_width and sclk_r = '0'
-           and counter_sclk = (ref_clk_freq_g / i2c_freq_g)/4-1 then -- Data transmission can only be done when the clock is low
+          if bits_counter_r = data_width_c and sclk_r = '0'
+           and counter_sclk_r = (ref_clk_freq_g / i2c_freq_g)/4-1 then -- Data transmission can only be done when the clock is low
             curr_state_r <= ack;
           else
             curr_state_r <= write_data;
@@ -156,8 +157,8 @@ begin -- rtl
                           --ACK--
 -----------------------------------------------------------------------
         when ack =>
-          if ack_flag = '1' or nack = '1'  then
-            if stop_request = '1' then
+          if ack_flag_r = '1' or nack_r = '1'  then
+            if stop_request_r = '1' then
               curr_state_r <= stop;        -- Once all the three bytes have been sent or a nack is received, we go to stop state
             else
               curr_state_r <= write_data;
@@ -169,8 +170,8 @@ begin -- rtl
                           --STOP--
 -----------------------------------------------------------------------
         when stop =>
-          if start_stop_done = '1' then
-            if param_counter = n_params_g then
+          if start_stop_done_r = '1' then
+            if param_counter_r = n_params_g then
               curr_state_r <= finish;     -- Only if all the parameters have been sent we can go to finish state
             else
               curr_state_r <= wait_start;
@@ -193,75 +194,74 @@ begin -- rtl
 
       if curr_state_r = wait_start then
         sdat_r <= '1';
-        if wait_counter = wait_delay-1 and sclk_r = '1' then --Wait delay is the buffer time between stop and start
-          wait_done <= '1';
-          wait_counter <= 0;
+        if wait_counter_r = wait_delay_c-1 and sclk_r = '1' then --Wait delay is the buffer time between stop and start
+          wait_done_r <= '1';
+          wait_counter_r <= 0;
         else
-          wait_counter <= wait_counter + 1;
+          wait_counter_r <= wait_counter_r + 1;
         end if;
       end if;
 
       if curr_state_r = start then
-        nack_done <= '0';
+        nack_done_r <= '0';
         if sclk_r = '1' then
-          start_stop_done <= '1';
+          start_stop_done_r <= '1';
           sdat_r <= '0';
-          wait_done <= '0';
-          nack <= '0';
+          wait_done_r <= '0';
+          nack_r <= '0';
         else
-          start_stop_done <= '0';
+          start_stop_done_r <= '0';
         end if;
-        if start_stop_done = '1' then
-          if wait_counter = wait_delay-1 and sclk_r = '1' then 
-            wait_done <= '1';  -- After we pull sdat low, we wait a prudential time before the data transmission
-            wait_counter <= 0;
+        if start_stop_done_r = '1' then
+          if wait_counter_r = wait_delay_c-1 and sclk_r = '1' then 
+            wait_done_r <= '1';  -- After we pull sdat low, we wait a prudential time before the data transmission
+            wait_counter_r <= 0;
           else
-            wait_counter <= wait_counter + 1;
+            wait_counter_r <= wait_counter_r + 1;
           end if;
         end if;
       end if;
       
       if curr_state_r = write_data then
-        wait_done <= '0';
-        ack_flag <= '0';
-        start_stop_done <= '0';
-        data_register_r <= data_transfer(n_params_g-param_counter-1);
-        byte_register_r <= data_register_r((bytes_sent-byte_counter)*data_width-1 downto (bytes_sent-byte_counter-1)*data_width);
-        if bits_counter /= data_width and bits_counter /= data_width+1 
-        and sclk_r = '0' and counter_sclk = (ref_clk_freq_g / i2c_freq_g)/4-1 then
-          sdat_r <= byte_register_r(data_width-bits_counter-1);
+        wait_done_r <= '0';
+        ack_flag_r <= '0';
+        start_stop_done_r <= '0';
+        data_register_r <= data_transfer_c(n_params_g-param_counter_r-1);
+        byte_register_r <= data_register_r((bytes_sent_c-byte_counter_r)*data_width_c-1 downto (bytes_sent_c-byte_counter_r-1)*data_width_c);
+        if bits_counter_r /= data_width_c and bits_counter_r /= data_width_c+1 
+        and sclk_r = '0' and counter_sclk_r = (ref_clk_freq_g / i2c_freq_g)/4-1 then
+          sdat_r <= byte_register_r(data_width_c-bits_counter_r-1);
         end if;
-        if sclk_r = '0' and counter_sclk = (ref_clk_freq_g / i2c_freq_g)/4-1 then
-          bits_counter <= bits_counter+1;
+        if sclk_r = '0' and counter_sclk_r = (ref_clk_freq_g / i2c_freq_g)/4-1 then
+          bits_counter_r <= bits_counter_r+1;
         end if;
       end if;
 
       if curr_state_r = ack then
         data_register_r <= (OTHERS => '0');
         byte_register_r <= (OTHERS => '0');
-        sdat_inout <= 'Z';
-        if sdat_inout = '1' and sclk_r = '1' and counter_sclk = (ref_clk_freq_g / i2c_freq_g)/2-1 then
-          nack <= '1';
-          stop_request <= '1';
-          byte_counter <= 0;
-          bits_counter <= 0;
+        if sdat_inout = '1' and sclk_r = '1' and counter_sclk_r = (ref_clk_freq_g / i2c_freq_g)/2-1 then
+          nack_r <= '1';
+          stop_request_r <= '1';
+          byte_counter_r <= 0;
+          bits_counter_r <= 0;
         else
-          nack <= nack;
+          nack_r <= nack_r;
         end if;
-        if sclk_r = '1' and counter_sclk = (ref_clk_freq_g / i2c_freq_g)/2-1 
-        and (nack = '0' and nack_done = '0') and sdat_inout = '0'  then
-          ack_flag <= '1';
+        if sclk_r = '1' and counter_sclk_r = (ref_clk_freq_g / i2c_freq_g)/2-1 
+        and (nack_r = '0' and nack_done_r = '0') and sdat_inout = '0'  then
+          ack_flag_r <= '1';
           sdat_r <= '0';
-          bits_counter <= 0;
-          if byte_counter /= bytes_sent-1 and nack = '0' then
-            byte_counter <= byte_counter + 1;
+          bits_counter_r <= 0;
+          if byte_counter_r /= bytes_sent_c-1 and nack_r = '0' then
+            byte_counter_r <= byte_counter_r + 1;
           end if;
-          if byte_counter = bytes_sent-1 then
-            stop_request <= '1';
-            byte_counter <= 0;
+          if byte_counter_r = bytes_sent_c-1 then
+            stop_request_r <= '1';
+            byte_counter_r <= 0;
           end if;
         else
-          ack_flag <= '0';
+          ack_flag_r <= '0';
         end if;
       end if;
       
@@ -272,23 +272,19 @@ begin -- rtl
       end if;
 
       if curr_state_r = stop then
-        start_stop_done <= '0';
-        stop_request <= '0';
-        --sdat_r <= '0';
-        --if nack = '1' then
-          --nack_done <= '1';
-       -- end if;
-        if sclk_r = '1' and counter_sclk = (ref_clk_freq_g / i2c_freq_g)/2-1 then
-          start_stop_done <= '1';
+        start_stop_done_r <= '0';
+        stop_request_r <= '0';
+        if sclk_r = '1' and counter_sclk_r = (ref_clk_freq_g / i2c_freq_g)/2-1 then
+          start_stop_done_r <= '1';
           sdat_r <= '1';
-          if nack = '1' then
-            param_counter <= param_counter;
-            nack_done <= '1';
+          if nack_r = '1' then
+            param_counter_r <= param_counter_r;
+            nack_done_r <= '1';
           else
-            param_counter <= param_counter + 1;
+            param_counter_r <= param_counter_r + 1;
           end if;
         else
-          start_stop_done <= '0';
+          start_stop_done_r <= '0';
         end if;
       end if;
       if curr_state_r = finish then
@@ -300,7 +296,7 @@ begin -- rtl
     end if;
 
   END PROCESS FSM_PROC;
-  
-param_status_out <= std_logic_vector(to_unsigned(param_counter,n_leds_g));
+
+param_status_out <= std_logic_vector(to_unsigned(param_counter_r,n_leds_g));
    
 end rtl;
